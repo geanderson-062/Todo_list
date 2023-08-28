0c76940669001125522bff52c3d7b90560784b60
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-08-28 09:43:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+//import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+  return (
+    <>
+
+    <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+
+     <div>Olá mundo</div>  
+
+    </>
+  )
+}
+
+export default App
