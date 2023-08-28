6665b14c5275f8c689a1c8eae1142d33d39b07f3
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-08-28 14:44:27 -0300

    table

--- src/Components/Tasklist.jsx
+++ src/Components/Tasklist.jsx
@@ -143,29 +143,48 @@ export default function Search() {
         </div>
       </div>
 
-      <div className="TaskBoard">
+      <div>
         {tasks.map((task, index) => (
           <>
-            <div className="card text-center">
-              <div className="card-header">{nametask[index]}</div>
-              <div className="card-body">
-                <p className="card-text">{task}</p>
-                <button
-                  href="#"
-                  className="btn btn-danger"
-                  onClick={() => {
-                    removeTask(index);
-                  }}
-                >
-                  Deletar
-                </button>
-              </div>
-              <div className="card-footer text-body-secondary">
-                Data de conclusão {date[index]}
-              </div>
-            </div>
-
+            <h3 className="fs-3">Suas tarefas</h3>
             <br />
+            <table className="table table-secondary table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Nome da tarefa</th>
+                  <th scope="col">Tarefa</th>
+                  <th scope="col">Data</th>
+                  <th scope="col">Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-dark">
+                  <th scope="row">{nametask[index]}</th>
+                  <td>{task}</td>
+                  <td>{date[index]}</td>
+                  <td>
+                    <button
+                      href="#"
+                      className="btn btn-danger"
+                      onClick={() => {
+                        removeTask(index);
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-trash-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                      </svg>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </>
         ))}
       </div>
