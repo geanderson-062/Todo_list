4ece8fe6821fc86638ae11251d352785056cbd1a
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-02 00:18:31 -0300

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from "react";
+import { useState } from "react";
 //bootstrapp
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
+
+import { FiSun, FiMoon } from "react-icons/fi";
 
 import Navbar from "./Components/Navbar";
 import Tasklist from "./Components/Tasklist";
 import Animation from "./Components/Animation";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  };
   return (
-    <>
+    <div className={`App ${darkMode ? "dark-mode" : ""}`}>
       <Navbar />
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button
+          style={{
+            marginTop: 20,
+            maxWidth: 50,
+            maxHeight: 50,
+            marginLeft: 30,
+          }}
+          onClick={toggleDarkMode}
+          class="btn btn-primary me-md-2"
+          type="button"
+        >
+          {darkMode ? <FiSun /> : <FiMoon />}
+        </button>
+      </div>
       <Animation />
       <Tasklist />
-    </>
+    </div>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,25 +4,27 @@ import "../styles/index.css";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar colorPrimary">
-        <div className="container-fluid">
-          <h1 className="navbar-brand fs-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              fill="currentColor"
-              className="bi bi-card-checklist"
-              viewBox="0 0 16 16"
-              style={{ marginRight: 10 }}
-            >
-              <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
-              <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
-            </svg>
-            Todo List
-          </h1>
-        </div>
-      </nav>
+      <header>
+        <nav className="navbar">
+          <div className="container-fluid">
+            <h1 className="navbar-brand fs-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-card-checklist"
+                viewBox="0 0 16 16"
+                style={{ marginRight: 10 }}
+              >
+                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
+              </svg>
+              Todo List
+            </h1>
+          </div>
+        </nav>
+      </header>
     );
   }
 }
