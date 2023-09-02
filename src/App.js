7407fbf4106c49354da5187144d8c73345978d82
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-02 16:50:10 -0300

    remove dark mode

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
 import React from "react";
-import { useState } from "react";
-//bootstrapp
+//libs
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
-
-import { FiSun, FiMoon } from "react-icons/fi";
-
+//conponents
 import Navbar from "./Components/Navbar";
 import Tasklist from "./Components/Tasklist";
 import Animation from "./Components/Animation";
 import Footer from "./Components/Footer";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    if (!darkMode) {
-      document.body.classList.add("dark-mode");
-    } else {
-      document.body.classList.remove("dark-mode");
-    }
-  };
   return (
-    <div className={`App ${darkMode ? "dark-mode" : ""}`}>
+    <>
       <Navbar />
-      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-        <button
-          style={{
-            maxWidth: 50,
-            maxHeight: 50,
-            marginLeft: "5%",
-          }}
-          onClick={toggleDarkMode}
-          class="btn btn-primary me-md-2"
-          type="button"
-        >
-          {darkMode ? <FiSun /> : <FiMoon />}
-        </button>
-      </div>
       <Animation />
       <Tasklist />
       <Footer />
-    </div>
+    </>
   );
 }
 
