2392b9370032ca08e4f50415d95277d68a61ed16
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-09-02 17:37:30 -0300

    modal tasks

--- src/Components/Tasklist.jsx
+++ src/Components/Tasklist.jsx
@@ -42,6 +42,7 @@ export default function Search() {
   const [indiceEdicaoTarefa, setIndiceEdicaoTarefa] = useState(null);
   const [showDeleteAllButton, setShowDeleteAllButton] = useState(false);
   const [showExportButton, setShowExportButton] = useState(false);
+  const [showTaskModal, setShowTaskModal] = useState(false);
 
   const adicionarTarefa = () => {
     if (
@@ -124,6 +125,10 @@ export default function Search() {
         confirmButtonColor: "#0D6EFD",
       });
     }
+  };
+
+  const abrirModalTarefa = () => {
+    setShowTaskModal(true);
   };
 
   const handleDateStartChange = (date) => {
@@ -528,18 +533,22 @@ export default function Search() {
                 <thead>
                   <tr>
                     <th scope="col">Tarefa</th>
-                    <th scope="col">Descrição</th>
-                    <th scope="col">Data de inicio</th>
-                    <th scope="col">Data de conclusão</th>
                     <th scope="col">Ações</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr className="bg-dark">
-                    <th scope="row">{nomeTarefa[index]}</th>
-                    <td>{tarefa}</td>
-                    <td>{dataStart[index]}</td>
-                    <td>{dataConclusion[index]}</td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target={`#TaskModal${index}`}
+                        onClick={() => abrirModalTarefa()}
+                      >
+                        {nomeTarefa[index]}
+                      </button>
+                    </td>
                     <td>
                       <button
                         href="#"
@@ -583,6 +592,111 @@ export default function Search() {
                   </tr>
                 </tbody>
               </table>
+
+              {/* Modal da tarefa */}
+              <div
+                className={`modal fade ${
+                  showTaskModal === index ? "show" : ""
+                }`}
+                style={{ display: showTaskModal === index ? "block" : "none" }}
+                id={`TaskModal${index}`} // Use o índice para tornar o ID único
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                tabIndex="-1"
+                aria-labelledby={`staticBackdropLabel${index}`} // Use o índice para tornar o ID único
+                aria-hidden={showTaskModal !== index}
+              >
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">
+                        {nomeTarefa[index]}
+                      </h1>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div class="modal-body">
+                      <table className="table table-secondary table-bordered table-hover">
+                        <thead>
+                          <tr>
+                            <th scope="col">Descrição</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr className="bg-dark">
+                            <td>{tarefa}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <table className="table table-secondary table-bordered table-hover">
+                        <thead>
+                          <tr>
+                            <th scope="col">Data de inicio</th>
+                            <th scope="col">Data de Conclusão</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr className="bg-dark">
+                            <td>{dataStart[index]}</td>
+                            <td>{dataConclusion[index]}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-dismiss="modal"
+                      >
+                        Ok
+                      </button>
+                      <button
+                        href="#"
+                        style={{ marginRight: 5 }}
+                        className="btn btn-success"
+                        data-bs-toggle="modal"
+                        data-bs-target={`#editModal${index}`}
+                        onClick={() => editarTarefa(index)}
+                        title="Editar Tarefa"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-pencil-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                        </svg>
+                      </button>
+                      <button
+                        href="#"
+                        style={{ marginRight: 5 }}
+                        className="btn btn-danger"
+                        onClick={() => confirmarRemocaoTarefa(index)}
+                        title="Excluir Tarefa"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-trash-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
               {/* Modal de Edição */}
               <div
